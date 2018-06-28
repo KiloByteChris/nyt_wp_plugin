19cd1bf3b6678db05c8bde2eee73b624b164ccb8
c.mcguire <cdmcguire000@gmail.com>
2018-06-28 16:20:20 -0700

    the display function now displays all the information

--- js/nyt.js
+++ js/nyt.js
@@ -4,28 +4,38 @@ var url = "https://api.nytimes.com/svc/topstories/v2/home.json";
 url += '?' + jQuery.param({
   'api-key': "c64d0a52048b48ec8c6f20d3f4930160"
 });
-console.log("hello");
 jQuery(document).ready(function(){
     // GET data from the API
     jQuery.ajax({
         url: url,
         method: 'GET',
     }).done(function(data) {
+        // select the div where the infomrtion will be displayed
         var artSection = jQuery('.nytTopStoriesDiv');
-        //Display the first 10 results
         console.log(data);
-        for (i=0 ; i<10 ; i++) {
+        /*
+            Function takes the nyt data and builds a string to display the infromation
+        */
+        function displayNYTData(i, data) {
+            var displayString = "<article>";
             // using moment.js, change the format of the date
             //var newDate = moment(data.results[i].created_date).format('YYYY MM DD');
-            // Create the articles using the API data
-            artSection.append('<article>' +
-                '<img src='+ data.results[i].multimedia[0].url +' />' +
-                '<h2><a href='+ data.results[i].url +'>'+ data.results[i].title +'</a></h2>' +
-                '<h3 class="storySection">'+ data.results[i].section +'</h3>' +
-                '<p class="abstract">'+ data.results[i].abstract +'</p>' +
-                //'<h3 class="date">'+ newDate +'</h3>' +
-                '<h3 class="byLine">'+ data.results[i].byline +'</h3>' +
-                '</article');
+            // Check for an image
+            if(typeof data.results[i].multimedia[0] !== 'undefined') {
+                displayString += "<img src="+ data.results[i].multimedia[0].url +" />";
+            }else{
+                displayString += "<img src='' />";
+            }
+            displayString += "<h2><a href="+ data.results[i].url +">"+data.results[i].title +" </a></h2>";
+            displayString += "<h3 class='storySection'>"+ data.results[i].section +"</h3>";
+            displayString += "<p class='abstract'>"+ data.results[i].abstract +"</p>";
+            //displayString += "<h3 class='date'>"+ newDate +"</h3>";
+            displayString += "<h3 class='byLine'>"+ data.results[i].byline +"</h3>";
+            displayString += "</article>";
+            artSection.append(displayString);
+        }
+        for (var i=0 ; i<10 ; i++) {
+            displayNYTData(i, data);
         }
     }).fail(function(err) {
         throw err;
